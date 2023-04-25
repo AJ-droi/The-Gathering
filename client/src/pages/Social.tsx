49ea4b9ca32a3e34c784a571ab2c6b75411575b1
AJ-droi <ajiriosiobe@gmail.com>
2023-04-25 22:59:17 +0100

    removed dependecy array

--- client/src/pages/Social.tsx
+++ client/src/pages/Social.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Social = () => {
     const navigate = useNavigate();
+    const [tok, setTok] = useState<boolean>(false);
     // const { setToken, getUser } = useContext(DataContext) as AllContext;
     const searchParams = new URLSearchParams(document.location.search);
     const token = searchParams.get('token')!;
@@ -10,15 +11,17 @@ const Social = () => {
       if (token !== null && token !== 'error') {
         localStorage.setItem('signature', token);
         // await getUser();
+        setTok(!tok)
         navigate('/dashboard');
       } else {
         navigate('/');
       }
     };
+
   
     useEffect(() => {
       getAndSetToken();
-    }, []);
+    }, );
   
     return null;
 }
